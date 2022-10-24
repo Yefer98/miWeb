7ca45c9a0268e0c7f468b8c681183de7acff72e4
Yefer98 <yyefer98@gmail.com>
2022-10-24 17:03:15 -0500

    submenu rotate

--- js/flecha.js
+++ js/flecha.js
@@ -1,7 +1,11 @@
-const links = document.querySelector('.links');
 
-links.addEventListener('onfocus',()=>{
-    const flecha = document.querySelector('.flecha');
-    
-    TransitionEvent.initTransitionEvent('transitioncancel');
-});
+function topla(){
+    const flecha = document.getElementById('flecha')
+    flecha.style.rotate = '180deg';
+}
+function topla2(){
+    const links = document.getElementById('links');
+    const flecha = document.getElementById('flecha');
+    flecha.style.rotate = '0deg';
+}
+
